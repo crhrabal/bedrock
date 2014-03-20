4015c5b58ca5e72420cd7942a423d7c7afaf45ed
Jon Petto <jonpetto@gmail.com>
2014-03-20 11:59:40 -0500

    Add firefox desktop pages. Bug 975156.

--- bedrock/settings/base.py
+++ bedrock/settings/base.py
@@ -185,6 +185,22 @@ MINIFY_BUNDLES = {
             'css/sandstone/video.less',
             'css/firefox/customize.less',
         ),
+        'firefox_desktop': (
+            'css/sandstone/sandstone-resp.less',
+            'css/firefox/desktop/index.less',
+        ),
+        'firefox_desktop_fast': (
+            'css/sandstone/sandstone-resp.less',
+            'css/firefox/desktop/fast.less',
+        ),
+        'firefox_desktop_customize': (
+            'css/sandstone/sandstone-resp.less',
+            'css/firefox/desktop/customize.less',
+        ),
+        'firefox_desktop_trust': (
+            'css/sandstone/sandstone-resp.less',
+            'css/firefox/desktop/trust.less',
+        ),
         'firefox_features': (
             'css/sandstone/video.less',
             'css/firefox/features.less',
@@ -511,6 +527,9 @@ MINIFY_BUNDLES = {
         'firefox_customize': (
             'js/base/mozilla-video-tools.js',
             'js/firefox/customize.js',
+        ),
+        'firefox_desktop_common': (
+            'js/firefox/desktop/common.js',
         ),
         'firefox_features': (
             'js/base/mozilla-video-tools.js',
